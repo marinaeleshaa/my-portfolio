499bc28e7b4e44ca5a1cdf38a144b5551c3626bc
marinaeleshaa <marinaeleshah77@gmail.com>
2025-11-29 23:59:54 +0200

    fix: separate the providers in another component feat: add redux store

---
+++ src/components/common/ClientProviders.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "@/redux/Store";
+import Dock from "@/components/common/Dock";
+import navData from "@/data/navData";
+
+export default function ClientProviders({ children }: any) {
+  const items = navData();
+
+  return (
+    <Provider store={store}>
+      <div className="relative min-h-screen">
+        {children}
+
+        <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2">
+          <Dock
+            items={items}
+            panelHeight={68}
+            baseItemSize={50}
+            magnification={70}
+            className="text-white"
+          />
+        </div>
+      </div>
+    </Provider>
+  );
+}
--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProjectsSection = () => {
   return (
-    <div>ProjectsSection</div>
+    <div className='max-w-6xl mx-auto px-4 md:px-8 lg:px-2'>ProjectsSection</div>
   )
 }
 
