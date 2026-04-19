e160248db90c6e3150bd109f2a84ca14ffdd8a90
marinaeleshaa <marinaeleshah77@gmail.com>
2026-04-19 17:28:34 +0200

    fix: adjust padding in AboutSection for better layout consistency

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -2,9 +2,9 @@ import AnimatedComputer from "@/components/about/AnimatedComputer";
 import LeftSide from "@/components/about/LeftSide";
 const AboutSection = () => {
   return (
-    <div className="bg-black  flex justify-center px-4">
+    <div className="bg-black  flex justify-center ">
       {/* Main wrapper */}
-      <div className="w-full  max-w-6xl mx-auto flex flex-col lg:flex-row items-center lg:items-center lg:justify-between ">
+      <div className="w-full px-4 md:px-8 lg:px-0  max-w-6xl mx-auto flex flex-col lg:flex-row items-center lg:items-center lg:justify-between ">
         <LeftSide />
 
         {/* Right Animation */}
